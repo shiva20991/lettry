a2d2fa6d3c96198b0600bf95776cab307b030561
M4R5 ᵐᵛᵖˡ <67886336+mvplID@users.noreply.github.com>
2022-05-20 06:55:12 +0700

    Update handle_start.py

--- bot/core/handlers/handle_start.py
+++ bot/core/handlers/handle_start.py
@@ -5,10 +5,8 @@ async def start_handler(message):
     user_id= message.sender_id
     chat_id= message.chat_id
     if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
-        msg = '''**Hello, ¡Welcome to Mirror RClone Bot\n\n
-I can help you transfer files from one cloud to another.\n
-Can also mirror files from telegram to cloud and leech from cloud to telegram**\n\n
+        msg = '''**Selamat datang di Mirror RClone Bot, Bot ini dapat membantumu mengunggah TG file ke penyimpanan awan ataupun sebaliknya; serta trasnfer antar penyimpanan awan.\n
 '''
         await message.reply(msg)
     else:
-        await message.reply('Not Authorized user, deploy your own version\n\nhttps://github.com/Sam-Max/Rclone-Tg-Bot')
+        await message.reply('Kamu tidak diijinkan!')
